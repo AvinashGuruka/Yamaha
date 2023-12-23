0c22ce150fe66265cc086cea94e00780684d37ef
Avinash Yadav <avinash.guruka@gmail.com>
2023-12-23 12:59:57 +0530

    Second commit

---
+++ src/Pulsar/MT.java
@@ -0,0 +1,8 @@
+package Pulsar;
+
+public class MT {
+	
+	public void mT() {
+		System.out.println("This is pulsar MT");
+	}
+}
--- src/Pulsar/Ns200.java
+++ src/Pulsar/Ns200.java
@@ -3,7 +3,7 @@ package Pulsar;
 public class Ns200 {
 	
 	public void nS() {
-		System.out.println("This is pulsar rs-200");	
+		System.out.println("This is pulsar rs-200");
 	}
 
 }
